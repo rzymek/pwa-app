a198faf096df8f04b3055e319e9c58efd92cf73c
rzymek <krzysztof.rzymkowski@gmail.com>
2026-04-01 22:03:43 +0200

    upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
-import {defineConfig} from "vite"
-import preact from "@preact/preset-vite"
-import {VitePWA} from "vite-plugin-pwa"
+import {defineConfig} from 'vitest/config'
+import preact from '@preact/preset-vite'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
